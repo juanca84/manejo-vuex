8a5a09b339df3792fcc9bddb982b094b2306f242
Juan Carlos Condori <juanc.com@gmail.com>
2020-05-26 19:48:19 -0400

    primer uso de estados con vuex

--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,15 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    msg: 'Vuex desde cero.'
   },
   mutations: {
   },
   actions: {
   },
   modules: {
+  },
+  getters: {
+
   }
 })
